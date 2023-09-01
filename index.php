ccabb3931146af01fd586890c168dd5692a23df5
Juan Vyctor <juanvyctor.sgo@gmail.com>
2023-09-01 09:27:55 -0300

    arrumado o index.php

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 
 session_start();
 
-include __DIR__ . 'Route.php';
-include __DIR__ . 'App.php';
+include __DIR__ . '/Route.php';
+include __DIR__ . '/App.php';
 include __DIR__ . '/roteador.php';
 include __DIR__ . '/database.php';
 include __DIR__ . '/auth.php';
