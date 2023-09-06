80181521af05cf40dab08e29bd2ba8605fd37af3
Juan Vyctor <juanvyctor.sgo@gmail.com>
2023-09-06 14:21:57 -0300

    criação do arquivo

---
+++ controllers/profile/updatename.php
@@ -0,0 +1,21 @@
+<?php
+    $method = $_SERVER['REQUEST_METHOD'];
+
+    if (!hasUser() || $method==='GET') {
+        header('Location: /');
+    } else {
+        $user_obj = new User(connection());
+        $email = $_SESSION['email'];
+        $password = $user_obj->getPassword($email);
+        $new_name = $POST['novo_nome'];
+
+        if ($password === $POST['confirma_senha']) {
+            $user_obj->updateName($new_name, $email);
+            header('Location: /perfil');
+        } else {
+            $password_error = true;
+        }
+
+        include __DIR__ . '/../../pages/profile/perfil.php'; 
+    }
+?>
