66b6fc9ebf87a778880ca9435cc2f961e4fa03d1
Juan Vyctor <juanvyctor.sgo@gmail.com>
2023-09-06 13:58:16 -0300

    retirado authorize()

--- auth.php
+++ auth.php
@@ -9,18 +9,4 @@ function logout () : void {
     unset($_SESSION['email']);
     session_destroy();
 }
-
-function authorize (Route $rota) {
-
-    if ($rota->getProfile() != '') {
-            $email = $_SESSION['email'];
-            $model = new User(connection());
-    
-            if ($model->find($email)) {            
-                return $model->getType($email) === 'admin';
-            }
-            return false;
-        }
-        return false;
-    }
 ?>
