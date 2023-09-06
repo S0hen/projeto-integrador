2a48cc214c95f8cccc373ef4f83d466a1e64e7ec
Juan Vyctor <juanvyctor.sgo@gmail.com>
2023-09-06 14:23:55 -0300

    criação da rota de updatepassword.php

--- roteador.php
+++ roteador.php
@@ -15,7 +15,8 @@ Route::get('/dashboard/mesas/agendamento', '/pages/mesas/agendamento.html', '');
 Route::get('/dashboard/mesas/criacao', '/pages/mesas/criacao.html', '');
 
 Route::get('/dashboard/user', '/controllers/profile/index.php', '');
-Route::post('/dashboard/user/update', '/controllers/profile/updatename.php', '');
+Route::post('/dashboard/user/update/nome', '/controllers/profile/updatename.php', '');
+Route::post('/dashboard/user/update/senha', '/controllers/profile/updatepassword.php', '');
 
 Route::post('/dashboard/mesas/criacao/criar', '/controllers/mesas/criar.php', '');
 Route::post('/dashboard/mesas/agendamento/agendar', '/controllers/mesas/agendar.php', '');
