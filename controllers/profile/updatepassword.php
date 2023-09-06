5e6c1182b817d05611a56bf91317d9f846cc7547
Juan Vyctor <juanvyctor.sgo@gmail.com>
2023-09-06 14:30:05 -0300

    criado updatepassword.php

---
+++ controllers/profile/updatepassword.php
@@ -0,0 +1,21 @@
+<?php
+    $method = $_SERVER['REQUEST_METHOD'];
+
+    if (!hasUser() || $method==='GET') {
+        header('Location: /');
+    } else {
+        $user_obj = new User(connection());
+        $email = $_SESSION['email'];
+        $new_password = $POST['nova_senha'];
+        $password = $user_obj->getPassword($email);
+
+        if ($password === $POST['confirma_senha']) {
+            $user_obj->updatePassword($new_password, $email);
+            header('Location: /perfil');
+        } else {
+            $password_error = true;
+        }
+
+        include __DIR__ . '/../../pages/profile/perfil.php'; 
+    }
+?>
