0363b2b054e60da600cb279eab60fac313172b28
Juan Vyctor <juanvyctor.sgo@gmail.com>
2023-09-08 23:46:37 -0300

    implementados casos de erro

--- controllers/profile/deleteuser.php
+++ controllers/profile/deleteuser.php
@@ -1,6 +1,15 @@
 <?php
+
+    $password_error = false;
+    $changed = false;
+    $password = $_POST['confirma_senha'];
+    $password2 = $_POST['confirma_senha_2'];
+
     if (!hasUser() || ($_SERVER['REQUEST_METHOD'] === 'GET')) {
         header('Location: /');
+    } else if ($password !== $password2) {
+        $password_error = true;
+        header('Location: /perfil');
     } else {
         $email = $_SESSION['email'];
         $model = new User(connection());
