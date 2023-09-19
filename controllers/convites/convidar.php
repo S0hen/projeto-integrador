4fca721abad2b86fcf59e20a9f36e23a110d5e69
Juan Vyctor <juanvyctor.sgo@gmail.com>
2023-09-19 16:25:23 -0300

    criado arquivo e alerta

--- controllers/convites/convidar.php
+++ controllers/convites/convidar.php
@@ -8,6 +8,16 @@ if (!hasUser()) {
         header('Location: /dashboard');
     }
 
+    if ($_GET['message']) {
+        $message = $_GET['message'];
+
+        if ($message == 'aceito') {
+            echo '<script type="text/javascript">';
+            echo ' alert("Convite enviado com sucesso.")';
+            echo '</script>';
+        }
+    }
+
     $mesa = Mesa::findById($_GET['mesa']);
     
     include ('pages/convites/convidar.php');
