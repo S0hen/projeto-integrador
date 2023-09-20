78f2a190a407935d28da86ba6f973d48a8488aab
Juan Vyctor <juanvyctor.sgo@gmail.com>
2023-09-20 15:13:52 -0300

    criado authorize()

--- auth.php
+++ auth.php
@@ -7,4 +7,19 @@ function logout () : void {
     unset($_SESSION['email']);
     session_destroy();
 }
+
+function authorize (Route $rota) {
+
+    if ($rota->getProfile() != '') {
+            $email = $_SESSION['email'];
+            $model = new User(connection());
+    
+            if ($model->find($email)) {            
+                return $model->getTipo($email) === 'adm';
+            }
+            return false;
+        }
+        return false;
+    }
+
 ?>
