09ee46ce8fcd6fd9589e57baec69cb9439edd4a8
Juan Vyctor <juanvyctor.sgo@gmail.com>
2023-09-29 17:04:19 -0300

    aplicadas alterações de Maria

--- pages/superuser/controle.php
+++ pages/superuser/controle.php
@@ -4,42 +4,72 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <!-- Add Bootstrap CSS link -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- Add your custom CSS if needed -->
+    <style>
+        .red-strip {
+            background-color: red;
+            height: 5px; 
+        }
+        .small-card {
+            padding: 10px; 
+        }
+        .small-card .card-body {
+            font-size: 14px;
+        }
+
+        .row {
+            padding-top: 30px;
+        }
+
+        .welcome-text {
+            text-align: center;
+            padding-top: 40px;
+        }
+
+        
+    </style>
 </head>
 <body>
-    <!-- página principal do superuser, o index.php vai dar include nessa aqui -->
-
-    <!-- foreach pra loopar todos os usuários, e nisso tem o botão que envia o email de cada Usuário por get -->
+  
     <div class="container">
-        <?php
-        foreach ($usuarios as $usuario) {
-            ?>
-            <div class="card mb-3">
-                <div class="card-body">
-                    <h5 class="card-title">Nome do usuário: <?= $usuario['usu_nome'] ?></h5>
-                    <h5 class="card-title">Email do usuário: <?= $usuario['usu_email'] ?></h5>
-                    <h5 class="card-title">Senha do usuário: <?= $usuario['usu_senha'] ?></h5>
-                    <a href="/dashboard/superuser/user?user_email=<?= urlencode($usuario['usu_email']) ?>" class="btn btn-secondary">Editar mesa</a>
+        <h1 class="welcome-text"> Bem vindo, admin. </h1>
+        <div class="row">
+            <!-- foreach pra loopar todos os usuários -->
+            <?php foreach ($usuarios as $usuario) { ?>
+            <div class="col-md-4">
+                <div class="card mb-3 small-card">
+                    <div class="red-strip"></div>
+                    <div class="card-body">
+                        <h5 class="card-title">Nome do usuário: <?= $usuario['usu_nome'] ?></h5>
+                        <h5 class="card-title">Email do usuário: <?= $usuario['usu_email'] ?></h5>
+                        <h5 class="card-title">Senha do usuário: <?= $usuario['usu_senha'] ?></h5>
+                        <a href="/dashboard/superuser/user?user_email=<?= urlencode($usuario['usu_email']) ?>" class="btn btn-secondary">Editar usuário</a>
+                    </div>
                 </div>
             </div>
-            <?php
-        } ?>
+            <?php } ?>
 
-    <!-- foreach pra loopar todas as mesas, e nisso tem o botão que envia o id de cada Mesa por get -->
-    <div class="container">
-        <?php
-        foreach ($mesas as $mesa) {
-            ?>
-            <div class="card mb-3">
-                <div class="card-body">
-                    <h5 class="card-title">Título da mesa: <?= $mesa['mes_titulo'] ?></h5>
-                    <p class="card-text">Descrição: <?= $mesa['mes_descricao'] ?></p>
-                    <a href="/dashboard/superuser/mesa/editar?mesa=<?= urlencode($mesa['mes_id']) ?>" class="btn btn-secondary">Editar mesa</a>
+            <!-- foreach pra loopar todas as mesas -->
+            <?php foreach ($mesas as $mesa) { ?>
+            <div class="col-md-4">
+                <div class="card mb-3 small-card">
+                    <div class="red-strip"></div>
+                    <div class="card-body">
+                        <h5 class="card-title">Título da mesa: <?= $mesa['mes_titulo'] ?></h5>
+                        <p class="card-text">Descrição: <?= $mesa['mes_descricao'] ?></p>
+                        <a href="/dashboard/superuser/mesa/editar?mesa=<?= urlencode($mesa['mes_id']) ?>" class="btn btn-secondary">Editar mesa</a>
+                    </div>
                 </div>
             </div>
-            <?php
-        } ?>
-
+            <?php } ?>
+        </div>
     </div>
 
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
