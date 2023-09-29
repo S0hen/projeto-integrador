69f9b132d543796cbd5ba9fdb3905ec7623f43c0
Juan-Vyctor <juanvyctor.sgo@gmail.com>
2023-09-29 09:38:57 -0300

    criadas verificacoes dos convites

--- controllers/convites/accept.php
+++ controllers/convites/accept.php
@@ -1,12 +1,23 @@
 <?php
 
-    $conId = $_GET['conId'];
+    //pega o id do usuário que tá logado e cria o objeto do convite pra fazer a checagem
+    $usuId = (new Usuarios(connection()))->getID($_SESSION['email']);
     $convite_obj = new Convites(connection());
-    $accept = $convite_obj->accept($conId);
+    $conId = $_GET['conId'];
     $convite = $convite_obj->findById($conId);
-    
-    $registro = Participacoes::save($convite['con_mes_id'], $convite['con_usu_id']);
 
-    header('Location:/dashboard/user/suasmesas/recebidos?message=aceito');
+    if((!hasUser()) || ($_SERVER['REQUEST_METHOD']) == 'POST') {
+        header('Location:/');
+    } else if ($usuId != $convite['con_usu_id']) {
+        //se quem tiver logado for diferente do dono do convite ele toma essa mensagem
+        echo '<h1>Este convite não está direcionado à você.</h1>';
+        echo '<a href="/dashboard/user/suasmesas">Voltar</a>';
+    } else {
+        //se passar por tudo ele aceita o convite
+        $accept = $convite_obj->accept($conId);
+        $registro = Participacoes::save($convite['con_mes_id'], $convite['con_usu_id']);
+
+        header('Location:/dashboard/user/suasmesas/recebidos?message=aceito');
+    }
 
 ?>
