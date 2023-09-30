de24619d913c2510d5d1878dff867b5edb9aab0d
Juan Vyctor <juanvyctor.sgo@gmail.com>
2023-09-30 08:12:53 -0300

    descomentei

--- controllers/convites/refuse.php
+++ controllers/convites/refuse.php
@@ -1,6 +1,5 @@
 <?php
 
-    //pega o id do usuário que tá logado e cria o objeto do convite pra fazer a checagem
     $usuId = (new Usuarios(connection()))->getID($_SESSION['email']);
     $convite_obj = new Convites(connection());
     $conId = $_GET['conId'];
@@ -9,11 +8,9 @@
     if((!hasUser()) || ($_SERVER['REQUEST_METHOD']) == 'POST') {
         header('Location:/');
     } else if ($usuId != $convite['con_usu_id']) {
-        //se quem tiver logado for diferente do dono do convite ele toma essa mensagem
         echo '<h1>Este convite não está direcionado à você.</h1>';
         echo '<a href="/dashboard/user/suasmesas">Voltar</a>';
     } else {
-        //se passar por tudo ele recusa o convite
         $refuse = (new Convites(connection()))->refuse($conId);
 
         header('Location:/dashboard/user/suasmesas/recebidos?message=recusado');
